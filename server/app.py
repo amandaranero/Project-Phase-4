b09d818c3920deba358e0e0972d6bed862dc6e3f
amanda ranero <amandaranero@gmail.com>
2023-05-12 18:04:22 -0400

    dotenv

--- server/app.py
+++ server/app.py
@@ -2,10 +2,12 @@ from flask import Flask, make_response, request, session, render_template
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 from models import db, Adopter, Agency, Dog, DogImage
+import uuid
+from dotenv import load_dotenv
+load_dotenv()
 import boto3
 import botocore
 import os
-import uuid
 
 
 
